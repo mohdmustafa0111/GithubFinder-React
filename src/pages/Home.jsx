f8ac2442f2d1bbb087daa52c721fa06bb8bb42ed
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2022-03-07 04:25:47 +0530

    got new token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
     return (
         <div>
-            <h1 className='text-6xl'>Welcome</h1>
-            {process.env.REACT_APP_GITHUB_TOKEN}
+            <UserResults />
         </div>
     )
 }
